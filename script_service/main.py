4bca7b71639d0763522e5c413d102e2041d321ac
synka777 <m.barbegayet@gmail.com>
2025-04-29 17:49:25 +0200

    Data sources download functional

--- script_service/main.py
+++ script_service/main.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import argparse
+import requests
 import init_db
-
+import json
+import os
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Script with optional --dev flag")
@@ -9,8 +11,27 @@ def parse_args():
     return parser.parse_args()
 
 
+def download_csv(url, dest):
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # Will overwrite the old csv files with updated ones next time it runs
+        with open(dest, 'wb') as file:
+            file.write(response.content)
+        return True
+    else:
+        print(f"\nFailed to download the file. Status code: {response.status_code}")
+        return False
+
+
+def load_sources(json_path):
+    with open(json_path) as j:
+        return json.load(j)
+
+
 def main():
-    parent_dir = Path(__file__).resolve().parent.parent
+    current_dir = Path(__file__).resolve().parent
+    parent_dir = current_dir.parent
     db_path = "/data/databrief.db" # Default: production (shared volume path)
 
     args = parse_args()
@@ -28,8 +49,17 @@ def main():
 
     # Proceed with the main logic
     print("Starting main logic...")
+    data_sources = load_sources(f"{current_dir}/sources.json")
+    tmp_dir = f"{current_dir}/tmp/"
 
+    if not Path.exists(Path(tmp_dir)):
+        os.mkdir(tmp_dir)
 
+    length = len(data_sources)
+    for i, ds in enumerate(data_sources):
+        print(f"Attempting CSV download {i+1}/{length}... ", end="")
+        if download_csv(ds["url"], f"{tmp_dir}{ds["type"]}.csv"):
+            print('OK')
 
 
 if __name__ == '__main__':
